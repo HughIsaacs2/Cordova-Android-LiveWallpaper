ab244ecab242a531da0d0fa80a0a389db903c768
Hugh Isaacs II <HughIsaacs2@users.noreply.github.com>
2016-09-06 11:50:50 -0400

    bam
    Bam!

---
+++ MyLiveWallpaper.java
@@ -0,0 +1,18 @@
+package com.hughisaacs2.cordova.plugins.androidlivewallpaperplugin;
+
+import android.annotation.TargetApi;
+import android.service.dreams.DreamService;
+import android.webkit.WebView;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.service.wallpaper.WallpaperService;
+import android.view.SurfaceHolder;
+
+@TargetApi(17)
+public class MyLiveWallpaper extends WallpaperService {
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Android Live Wallpaper Plugin for Cordova
+==============================
+
+I'm far from done, so this isn't to be used by anyone yet. I'm just sharing the code in case you wish to tear it apart and build you own or whatever.
+
+License
+-------
+
+This has been released under MIT license; see LICENSE for details.
